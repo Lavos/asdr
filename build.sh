5dbf8ea3c8309ab653da7918837876d5df35ea9e
Kristopher Cost <me@kriscost.com>
2013-06-03 14:49:58 -0700

    master: organization

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 case $1 in
 	prod) cat ./bin/first.min.js ./bin/modules/*.js ./bin/last.min.js;; 
-	dev) cat ./bin/first.js ./src/modules/*.js ./bin/last.js;;
+	dev) cat ./src/first.js ./src/modules/*.js ./src/last.js;;
 esac
