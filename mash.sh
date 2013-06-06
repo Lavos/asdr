1bc3b734c4e0144b2659f7e4d75a9be8faf453d0
Kristopher Cost <me@kriscost.com>
2013-06-06 15:56:30 -0700

    master: push script now rsyncs to the netstorage

--- mash.sh
+++ mash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 case $1 in
-	prod) cat ./bin/first.min.js ./bin/modules/*.js ./bin/last.min.js;; 
+	prod) cat ./bin/first.min.js ./bin/modules/*.js ./bin/last.min.js;;
 	dev) cat ./src/first.js ./src/modules/*.js ./src/last.js;;
 esac
--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@ FILE=$1
 CP_CODE=228070
 URL="spinmedia123.upload.akamai.com"
 
-echo "rsync -av -e \"ssh -i ~/ssh/kris.buzzmedia.private.key\" \"$FILE\" sshacs@$URL:/$CP_CODE/"
+rsync -av -e "ssh -i ./kris.buzzmedia.private.key" "$FILE" sshacs@$URL:/$CP_CODE/
