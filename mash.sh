5253d8d2ac2d14d46fd72f61fdec2c65dbb3acdc
Kristopher Cost <me@kriscost.com>
2013-06-07 10:19:37 -0700

    master: adding date and current sha of the branch to the mashed file

--- mash.sh
+++ mash.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "// $(date)"
+echo "// $(git rev-parse HEAD)"
+
 case $1 in
 	prod) cat ./bin/first.min.js ./bin/modules/*.js ./bin/last.min.js;;
 	dev) cat ./src/first.js ./src/modules/*.js ./src/last.js;;
