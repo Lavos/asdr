717c318b28b4bb07a4336e75753cac971bce5352
Kristopher Cost <me@kriscost.com>
2013-08-12 16:37:48 -0700

    master: code tweaks and optimizations to videotoaster, fixes to object css changes, removing rudundant code

--- src/modules/videotoaster.js
+++ src/modules/videotoaster.js
@@ -1,61 +1,75 @@
-CLARITY.provide('videotoaster', ['jquery','doubleunderscore'], function($, __){
+CLARITY.provide('videotoaster', ['jquery', 'underscore', 'doubleunderscore'], function($, _, __){
+	var defaults = {
+		type: 'youtube',
+		src: '',
+		height: 200,
+		width: 300,
+		padding: 30,
+		brandingImg: '',
+		brandingClick: '',
+		brandinglocation: [-10, 0],
+		tracking: '',
+		extraCSS: ''
+	};
+
 	//return functions
 	return {
-		init: function(config){
-			//check if videotoaster exists, animate out if yes
-			if($('.videotoaster').length){
-				$('.videotoaster').animate({bottom:'-='+($('.videotoaster').height()+30)+''},'slow',function(){ this.remove(); });
-			}
+		init: function(params){
+			var config = {};
+			_.defaults(config, params, defaults);
+
+			// kill any currently existing videotoasters
+			this.kill();
+
 			//variables
-			var cachebreak = (+new Date());
-			var vtID = 'bmVideoToaster' + cachebreak, exitID = 'vtExit' + cachebreak,
-				vtCSS = 'width='+ config.width +'px; height='+config.height+'px; position:fixed; bottom:-'+(config.height+30)+'px; right:15%; border:1px solid #b0b0b0; z-index:100;' + config.extraCSS;
+			var cachebreak = +new Date();
+			var vtID = 'bmVideoToaster' + cachebreak,
+				vtCSS = 'width='+ config.width +'px; height='+config.height+'px; position:fixed; bottom:-'+(config.height+config.padding)+'px; right:15%; border:1px solid #b0b0b0; z-index:100;' + config.extraCSS;
+
 			if(config.type == "youtube") {
-				$videotoaster = $('<div class="videotoaster" style="'+vtCSS+'"><div id="'+vtID+'">You need Flash player 8+ and JavaScript enabled to view this video.</div><i class="icon-remove-sign" id="'+exitID+'" style="position:absolute;top:3px;right:3px;cursor:pointer;color:#ff0000;z-index:101"></i><a href="'+config.brandingClick+'" target="_blank"><img src="'+config.brandingImg+'" border="0" style="position:absolute;top:'+config.brandinglocation[0]+'px;left:'+config.brandinglocation[1]+'px;z-index:101" /></a><img src="'+config.tracking+cachebreak+'" style="display:none"/></div>');
+				$videotoaster = $('<div class="videotoaster" style="'+vtCSS+'"><div id="'+vtID+'">You need Flash player 8+ and JavaScript enabled to view this video.</div><i class="icon-remove-sign videotoaster_remove" style="position:absolute;top:3px;right:3px;cursor:pointer;color:#ff0000;z-index:101"></i><a href="'+config.brandingClick+'" target="_blank"><img src="'+config.brandingImg+'" border="0" style="position:absolute;top:'+config.brandinglocation[0]+'px;left:'+config.brandinglocation[1]+'px;z-index:101" /></a><img src="'+config.tracking+cachebreak+'" style="display:none"/></div>');
 				$('body').append($videotoaster);
-				//if swfobject not on page, add it and run it
-				if(typeof swfobject !== "object") {
-					__.addJS('http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js',function(){
-						var params = { allowScriptAccess: "always" };
-						var atts = { id: vtID };
-						srcID = config.src.split('/');
-						swfobject.embedSWF("http://www.youtube.com/v/"+srcID[3]+"?enablejsapi=1&playerapiid="+vtID+"&version=3", vtID, config.width, config.height, "8", null, null, params, atts);	
-						//extra CSS to Object
-						$('#'+vtID).css({'display':'block'});
-					});
-				} else {
+
+				function processSWFObject () {
 					var params = { allowScriptAccess: "always" };
 					var atts = { id: vtID };
 					srcID = config.src.split('/');
-					swfobject.embedSWF("http://www.youtube.com/v/"+srcID[3]+"?enablejsapi=1&playerapiid="+vtID+"&version=3", vtID, config.width, config.height, "8", null, null, params, atts);	
-					//extra CSS to Object
-					$('#'+vtID).css({'display':'block'});
+					swfobject.embedSWF("http://www.youtube.com/v/"+srcID[3]+"?enablejsapi=1&playerapiid="+vtID+"&version=3", vtID, config.width, config.height, "8", null, null, params, atts, function(e){
+						//extra CSS to Object
+						e.ref.style.display = 'block';
+					});
+				};
+
+				//if swfobject not on page, add it and run it
+				if (typeof swfobject !== 'object') {
+					__.addJS('http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js', processSWFObject);
+				} else {
+					processSWFObject();
 				}
-				if(typeof window.onYouTubePlayerReady == "function") {
-					var old_onYouTubePlayerReady = window.onYouTubePlayerReady();
-				}
+
+				var old_onYouTubePlayerReady = (typeof window.onYouTubePlayerReady == 'function' ? window.onYouTubePlayerReady : function(){});
+
 				window.onYouTubePlayerReady = function onYouTubePlayerReady(playerId) {
-					if(typeof playerId !== 'undefined' && playerId.indexOf('bmVideoToaster' + cachebreak) >= 0) {
+					if (typeof playerId !== 'undefined' && playerId.indexOf('bmVideoToaster' + cachebreak) >= 0) {
 						ytplayer = document.getElementById(playerId);
 						ytplayer.playVideo();
 						ytplayer.mute();
-					} else if (typeof old_onYouTubePlayerReady == 'function') {
+					} else {
 						old_onYouTubePlayerReady();
 					}
 				}
-			} else if (config.type == "iframe") {
-				$videotoaster = $('<div class="videotoaster" style="'+vtCSS+'"><iframe id="'+vtID+'" src="'+config.src+'" scrolling="no" frameborder="0" tabindex="0" allowtransparency="true" width="'+config.width+'" height="'+config.height+'"></iframe><i class="icon-remove-sign" id="'+exitID+'" style="position:absolute;top:3px;right:3px;cursor:pointer;color:#ff0000;z-index:101"></i><a href="'+config.brandingClick+'" target="_blank"><img src="'+config.brandingImg+'" border="0" style="position:absolute;top:'+config.brandinglocation[0]+'px;left:'+config.brandinglocation[1]+'px;z-index:101" /></a><img src="'+config.tracking+cachebreak+'" style="display:none"/></div>');
+			} else if (config.type == 'iframe') {
+				$videotoaster = $('<div class="videotoaster" style="'+vtCSS+'"><iframe id="'+vtID+'" src="'+config.src+'" scrolling="no" frameborder="0" tabindex="0" allowtransparency="true" width="'+config.width+'" height="'+config.height+'"></iframe><i class="icon-remove-sign videotoaster_remove" style="position:absolute;top:3px;right:3px;cursor:pointer;color:#ff0000;z-index:101"></i><a href="'+config.brandingClick+'" target="_blank"><img src="'+config.brandingImg+'" border="0" style="position:absolute;top:'+config.brandinglocation[0]+'px;left:'+config.brandinglocation[1]+'px;z-index:101" /></a><img src="'+config.tracking+cachebreak+'" style="display:none"/></div>');
 					$('body').append($videotoaster);
 			}
 			//animate in
-			$videotoaster.animate({bottom:'+='+(config.height+30)+''},'slow');
+			$videotoaster.animate({bottom:'+='+(config.height+config.padding)+''},'slow');
+
 			//exit button
-			$('#'+exitID).click(function(){
-				$videotoaster.animate({bottom:'-='+(config.height+30)+''},'slow',function(){ $videotoaster.remove(); vt = ""; });
-			});
+			$videotoaster.on('click', '.videotoaster_remove', this.kill);
 		},
 		kill: function(){ //kill all toasters
-			$('.videotoaster').each(function(){ $(this).animate({bottom:'-='+($(this).height()+30)+''},'slow',function(){ this.remove(); }); });
-		}				
+			$('.videotoaster').each(function(){ $(this).animate({bottom:'-='+($(this).height()+config.padding)+''},'slow',function(){ this.remove(); }); });
+		}
 	};
 });
